84e602612c86819a38c75cf2d912b2b55b5630c9
Bartosz Nowak <bartekn337@gmail.com>
2024-09-13 21:31:21 +0200

    stark proof verification

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,31 @@
+use solana_program::entrypoint;
 use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
+    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
     pubkey::Pubkey,
-    msg,
 };
+use swiftness_air::layout::recursive::Layout;
+use swiftness_stark::types::StarkProof;
 
 // declare and export the program's entrypoint
 entrypoint!(process_instruction);
- 
+
 // program entrypoint's implementation
 pub fn process_instruction(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    _instruction_data: &[u8]
+    instruction_data: &[u8],
 ) -> ProgramResult {
     // log a message to the blockchain
-    msg!("Hello, world!");
- 
-    // gracefully exit the program
+    msg!("Stark verifier");
+
+    let stark_proof: StarkProof =
+        bincode::deserialize(instruction_data).map_err(|_| ProgramError::InvalidArgument)?;
+    let security_bits = stark_proof.config.security_bits();
+    let result = stark_proof
+        .verify::<Layout>(security_bits)
+        .map_err(|_| ProgramError::InvalidArgument)?;
+
+    msg!("Proof verified {:?}", result);
+
     Ok(())
 }
