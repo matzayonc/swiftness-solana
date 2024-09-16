0ee8e5220cfa0d0a1a84a0bab2a47c0af324266d
Bartosz Nowak <bartekn337@gmail.com>
2024-09-16 17:38:59 +0200

    proof uploading works

--- client/src/main.rs
+++ client/src/main.rs
@@ -31,40 +31,59 @@ enum AllocatorRequest {
     AllocateRequest(AllocateRequest),
 }
 
-struct TransactionManager<'a> {
-    client: &'a RpcClient,
+// Function to return a Vec of futures for tracking transactions
+async fn send_transactions(
+    client: &RpcClient,
+    transactions: Vec<Transaction>,
+) -> Vec<Result<Signature, solana_rpc_client_api::client_error::Error>> {
+    let mut futures = FuturesUnordered::new();
+
+    for (idx, tx) in transactions.iter().enumerate() {
+        sleep(Duration::from_millis(100));
+        // Wrap each transaction in a future and track the result
+        let future = async move { (idx, client.send_transaction(tx).await) };
+        futures.push(future);
+    }
+
+    let mut results = Vec::new();
+
+    while let Some(res) = futures.next().await {
+        println!("{:?}", res);
+        results.push(res.1)
+    }
+
+    results
 }
 
-impl<'a> TransactionManager<'a> {
-    fn new(client: &'a RpcClient) -> Self {
-        Self { client }
-    }
+/// Creates a `Transaction` to create an account with rent exemption
+async fn create_proof_data_account(
+    client: &RpcClient,
+    payer: &Keypair,
+    proof_data_account: &Keypair,
+    proof_size: usize,
+    owner: &Pubkey,
+) -> Result<Transaction, Box<dyn std::error::Error>> {
+    let rent_exemption_amount = client
+        .get_minimum_balance_for_rent_exemption(proof_size)
+        .await?;
 
-    // Function to return a Vec of futures for tracking transactions
-    async fn send_all_transactions(
-        &self,
-        transactions: Vec<Transaction>,
-    ) -> Vec<Result<Signature, solana_rpc_client_api::client_error::Error>> {
-        let mut futures = FuturesUnordered::new();
+    let create_account_ix = system_instruction::create_account(
+        &payer.pubkey(),
+        &proof_data_account.pubkey(),
+        rent_exemption_amount,
+        proof_size as u64,
+        owner,
+    );
 
-        for (idx, tx) in transactions.iter().enumerate() {
-            // Wrap each transaction in a future and track the result
-            sleep(Duration::from_millis(10));
-            let future = async move { (idx, self.client.send_and_confirm_transaction(tx).await) };
-            println!("Transaction {:?} requested", idx);
-            futures.push(future);
-        }
+    let blockhash = client.get_latest_blockhash().await?;
+    let tx = Transaction::new_signed_with_payer(
+        &[create_account_ix],
+        Some(&payer.pubkey()),
+        &[payer, proof_data_account],
+        blockhash,
+    );
 
-
-        let mut results = Vec::new();
-
-        while let Some(res) = futures.next().await {
-            println!("{:?}", res);
-            results.push(res.1)
-        }
-
-        results
-    }
+    Ok(tx)
 }
 
 #[tokio::main]
@@ -74,60 +93,72 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stark_proof = &std::fs::read(cli.serialized_proof)?;
     let allocator_id = Pubkey::from_str("6w9j57UyR8TT9PhtaiRBV4GV99DUjXcDyqVmEde8FHxa").unwrap();
     let payer = Keypair::read_from_file("/home/bartosz/.config/solana/id.json").unwrap();
+
     let proof_data_account = Keypair::new();
     println!("account pubkey: {:?}", proof_data_account.pubkey());
-
-    let rent_exemption_amount = client
-        .get_minimum_balance_for_rent_exemption(stark_proof.len())
-        .await?;
-    let ix = system_instruction::create_account(
-        &payer.pubkey(),
-        &proof_data_account.pubkey(),
-        rent_exemption_amount,
-        stark_proof.len() as u64,
-        &allocator_id,
-    );
-    let create_account_tx = Transaction::new_signed_with_payer(
-        &[ix],
-        Some(&payer.pubkey()),
-        &[&payer, &proof_data_account],
-        client.get_latest_blockhash().await?,
-    );
     client
-        .send_and_confirm_transaction(&create_account_tx)
+        .send_and_confirm_transaction(
+            &create_proof_data_account(
+                &client,
+                &payer,
+                &proof_data_account,
+                stark_proof.len(),
+                &allocator_id,
+            )
+            .await?,
+        )
         .await?;
 
-    // Allocate data instructions
-    let instructions: Vec<Instruction> = stark_proof.chunks(100).enumerate().map(|(idx, chunk)| Instruction {
-        program_id: allocator_id,
-        accounts: vec![AccountMeta::new(proof_data_account.pubkey(), false)],
-        data: bincode::serialize(&AllocatorRequest::AllocateRequest(AllocateRequest {
-            idx_start: idx*100,
-            data: chunk.to_vec(),
-        }))
-        .unwrap(),
-    }).collect();
+    for (index, chunk) in stark_proof.chunks(10000).enumerate() {
+        // Allocate data instructions
+        let instructions: Vec<Instruction> = chunk
+            .chunks(500)
+            .enumerate()
+            .map(|(idx, chunk)| {
+                let i = idx * 500 + index * 10000;
+                Instruction {
+                    program_id: allocator_id,
+                    accounts: vec![AccountMeta::new(proof_data_account.pubkey(), false)],
+                    data: bincode::serialize(&AllocatorRequest::AllocateRequest(AllocateRequest {
+                        idx_start: i,
+                        data: chunk.to_vec(),
+                    }))
+                    .unwrap(),
+                }
+            })
+            .collect();
 
-    // Create corresponding transactions
-    let block_hash = client.get_latest_blockhash().await?;
-    let mut txs = vec![];
-    for ix in instructions {
-        let tx = Transaction::new_signed_with_payer(
-            &[ix],
-            Some(&payer.pubkey()),
-            &[&payer],
-            block_hash
-        );
-        txs.push(tx);
+        // Create corresponding transactions
+        let block_hash = client.get_latest_blockhash().await?;
+        let mut txs = vec![];
+        for ix in instructions {
+            let tx = Transaction::new_signed_with_payer(
+                &[ix],
+                Some(&payer.pubkey()),
+                &[&payer],
+                block_hash,
+            );
+            txs.push(tx);
+        }
+
+        let _results = send_transactions(&client, txs).await;
+
+        sleep(Duration::from_millis(200));
     }
 
-    // Create a transaction manager
-    let manager = TransactionManager::new(&client);
+    loop {
+        let data = client
+            .get_account_data(&proof_data_account.pubkey())
+            .await?;
 
-    // Send all transactions and track progress
-    let _results = manager.send_all_transactions(txs).await;
+        if data.eq(stark_proof) {
+            println!("proof_data_account correct!");
+            break;
+        } else {
+            println!("proof_data_account data not maching!");
+            sleep(Duration::from_secs(5));
+        }
+    }
 
-    println!("{:?}", client.get_account(&proof_data_account.pubkey()).await?);
-    
     Ok(())
 }
