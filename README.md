fe11643356249493f013abf026bc6865d8e35e4f
Mateusz Zając <matzayonc@gmail.com>
2025-01-22 23:55:05 +0100

    saved serialized proof on solana

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
+# Swiftness Solana Program
+
 Program Id: x7dqKWiBUcWXzRVjdK3UeeFMPiRDivhrnmt2hoCZbwA
+
+## Local Development
+
+Start Local Solana Validator and set it as default endpoint
+
+```bash
+solana-test-validator
+solana config set -u localhost
+```
+
+Create a new account and request funds from the local validator
+
+```bash
+solana-keygen new
+solana airdrop 100
+```
+
+Build Program
+
+```bash
+cargo build-bpf
+```
+
+Build and deploy Program
+
+```bash
+cargo build-bpf && solana program deploy target/deploy/swiftness.so
+```
+
+Notice:
+
+- Program id has to be hardcoded in `src/lib.rs`
+- Program id will change after each build on a new machine
+
+## Usage
+
+Run client to send and verify an example proof
+
+```bash
+cargo run --example client
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::str::FromStr;
+
+use serde::{Deserialize, Serialize};
+use solana_program::account_info::next_account_info;
 use solana_program::entrypoint;
 use solana_program::{
     account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
@@ -9,20 +13,59 @@ use swiftness_stark::types::StarkProof as StarkProofVerifier;
 // declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
+pub const PROGRAM_ID: &str = "9yjazUFyg5nPA24oq1ErcuNhPEmNXqS6swBfu91RRqB7";
+
+#[repr(u8)]
+#[derive(Serialize, Deserialize)]
+pub enum Entrypoint<'a> {
+    PublishFragment { offset: usize, data: &'a [u8] },
+    VerifyProof {},
+}
+
 // program entrypoint's implementation
 pub fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    program_id: &Pubkey,
+    account_info: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let stark_proof: StarkProofVerifier =
-        bincode::deserialize(instruction_data).map_err(|_| ProgramError::InvalidArgument)?;
-    let security_bits = stark_proof.config.security_bits();
-    let result = stark_proof
-        .verify::<Layout>(security_bits)
-        .map_err(|_| ProgramError::InvalidArgument)?;
+    // if program_id != &Pubkey::from_str(PROGRAM_ID).unwrap() {
+    //     return Err(ProgramError::Custom(1));
+    // }
+    msg!("start: {:?}", program_id);
+    let instruction: Entrypoint = bincode::deserialize(instruction_data).unwrap();
+    msg!("instruction");
+    let accounts_iter = &mut account_info.iter();
 
-    msg!("Proof verified {:?}", result);
+    msg!("accounts_iter");
+
+    let account = next_account_info(accounts_iter).unwrap();
+    msg!("account");
+    assert!(account.is_writable == true);
+    let mut account_data = account.try_borrow_mut_data()?;
+    msg!("account_data");
+    // account_data[0] = 7;
+
+    match instruction {
+        Entrypoint::PublishFragment { offset, data } => {
+            account_data[offset..offset + data.len()].copy_from_slice(&data);
+            msg!("PublishFragment");
+        }
+        Entrypoint::VerifyProof {} => {
+            let stark_proof: StarkProofVerifier = bincode::deserialize(&account_data).unwrap();
+            msg!("VerifyProof");
+        }
+    }
+
+    msg!("done");
+
+    // let stark_proof: StarkProofVerifier =
+    //     bincode::deserialize(instruction_data).map_err(|_| ProgramError::InvalidArgument)?;
+    // let security_bits = stark_proof.config.security_bits();
+    // let result = stark_proof
+    //     .verify::<Layout>(security_bits)
+    //     .map_err(|_| ProgramError::InvalidArgument)?;
+
+    // msg!("Proof verified {:?}", result);
 
     Ok(())
 }
@@ -30,12 +73,12 @@ pub fn process_instruction(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use swiftness::{types::StarkProof as StarkProofVerifier, TransformTo};
+    use swiftness::{stark_proof, types::StarkProof as StarkProofVerifier, TransformTo};
     use swiftness_proof_parser::parse;
 
     #[test]
     fn test_deserialize_proof() {
-        let instruction_data = include_bytes!("../serializer/proof.bin");
+        let instruction_data = include_bytes!("../resources/proof.bin");
 
         let stark_proof: StarkProofVerifier = bincode::deserialize(instruction_data)
             .map_err(|_| ProgramError::InvalidArgument)
@@ -50,11 +93,12 @@ mod tests {
 
     #[test]
     fn test_verify_proof() {
-        let small_json = include_str!("../serializer/small.json");
-        let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
-
-        // let proof_bytes = bincode::serialize(&stark_proof).unwrap();
-        // let stark_proof: StarkProofVerifier = bincode::deserialize(&proof_bytes).unwrap();
+        let small_json = include_str!("../resources/small.json");
+        let stark_proof = parse(small_json.to_string()).unwrap();
+        let proof_bytes = bincode::serialize(&stark_proof).unwrap();
+        dbg!(&proof_bytes.len());
+        let stark_proof: stark_proof::StarkProof = bincode::deserialize(&proof_bytes).unwrap();
+        let stark_proof: StarkProofVerifier = stark_proof.transform_to();
 
         let security_bits = stark_proof.config.security_bits();
 
