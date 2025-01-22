388b10e8738666be4b7aaa8337b8a96fc9e756f8
Mateusz Zając <matzayonc@gmail.com>
2025-01-22 13:36:52 +0100

    verification in test

--- serializer/src/main.rs
+++ serializer/src/main.rs
@@ -1,7 +1,9 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use swiftness::{types::StarkProof as StarkProofVerifier, TransformTo};
+use swiftness::{
+    config, types::StarkProof as StarkProofVerifier, PublicInput, StarkWitness, TransformTo,
+};
 use swiftness_proof_parser::parse;
 
 #[derive(Parser)]
@@ -20,7 +22,64 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = SolanaClient::parse();
     let stark_proof: StarkProofVerifier =
         parse(std::fs::read_to_string(cli.proof)?)?.transform_to();
-    std::fs::write(cli.output, &bincode::serialize(&stark_proof).unwrap()).unwrap();
+
+    let serialized = bincode::serialize(&stark_proof).unwrap();
+    let stark_proof: StarkProofVerifier = bincode::deserialize(&serialized).unwrap();
+    // std::fs::write(&cli.output, &serialized).unwrap();
+
+    // let instruction_data_from_file = std::fs::read(cli.output)?;
 
     Ok(())
+}
+
+#[test]
+fn test_stark_proof() {
+    let small_json = include_str!("../small.json");
+    let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+    let serialized = bincode::serialize(&stark_proof).unwrap();
+    println!("proof size: {:?}", serialized.len());
+    let _stark_proof: StarkProofVerifier = bincode::deserialize(&serialized).unwrap();
+}
+
+#[test]
+fn test_stark_config() {
+    let small_json = include_str!("../small.json");
+    let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+    let serialized = bincode::serialize(&stark_proof.config).unwrap();
+    println!("config size: {:?}", serialized.len());
+    let _stark_proof: config::StarkConfig = bincode::deserialize(&serialized).unwrap();
+}
+
+#[test]
+fn test_stark_public_input() {
+    let small_json = include_str!("../small.json");
+    let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+    let serialized = bincode::serialize(&stark_proof.public_input).unwrap();
+    println!("public input size: {:?}", serialized.len());
+    let _stark_proof: swiftness_air::public_memory::PublicInput =
+        bincode::deserialize(&serialized).unwrap();
+}
+
+#[test]
+fn test_stark_unsent_commitment() {
+    let small_json = include_str!("../small.json");
+    let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+    let serialized = bincode::serialize(&stark_proof.unsent_commitment).unwrap();
+    println!("unsent commitment size: {:?}", serialized.len());
+    let _stark_proof: swiftness::types::StarkUnsentCommitment =
+        bincode::deserialize(&serialized).unwrap();
+}
+
+#[test]
+fn test_stark_witness() {
+    let small_json = include_str!("../small.json");
+    let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+    let serialized = bincode::serialize(&stark_proof.witness).unwrap();
+    println!("witness size: {:?}", serialized.len());
+    let _stark_proof: swiftness::types::StarkWitness = bincode::deserialize(&serialized).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,42 @@ pub fn process_instruction(
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use swiftness::{types::StarkProof as StarkProofVerifier, TransformTo};
+    use swiftness_proof_parser::parse;
+
+    #[test]
+    fn test_deserialize_proof() {
+        let instruction_data = include_bytes!("../serializer/proof.bin");
+
+        let stark_proof: StarkProofVerifier = bincode::deserialize(instruction_data)
+            .map_err(|_| ProgramError::InvalidArgument)
+            .unwrap();
+        let security_bits = stark_proof.config.security_bits();
+
+        let _result = stark_proof
+            .verify::<Layout>(security_bits)
+            .map_err(|_| ProgramError::InvalidArgument)
+            .unwrap();
+    }
+
+    #[test]
+    fn test_verify_proof() {
+        let small_json = include_str!("../serializer/small.json");
+        let stark_proof: StarkProofVerifier = parse(small_json.to_string()).unwrap().transform_to();
+
+        // let proof_bytes = bincode::serialize(&stark_proof).unwrap();
+        // let stark_proof: StarkProofVerifier = bincode::deserialize(&proof_bytes).unwrap();
+
+        let security_bits = stark_proof.config.security_bits();
+
+        let (program_hash, _output) = stark_proof.verify::<Layout>(security_bits).unwrap();
+        assert_eq!(
+            program_hash.to_hex_string(),
+            "0x2820cfb261b9ffa9f5fe7af15ff3d4df545154f26bfd4f234d1f6ba18171157"
+        );
+    }
+}
